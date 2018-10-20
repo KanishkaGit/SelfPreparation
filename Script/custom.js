9af03092ed0ea7e567b8386b700923e0d2e2e137
Kanishka Guha Biswas <kguhabiswas@gmail.com>
2018-10-20 22:53:14 +0530

    New Changes with sub pages

--- Script/custom.js
+++ Script/custom.js
@@ -1,26 +1,17 @@
 function onload() {
-    var id = getQueryStringValue("id");
-    getRoute(id);
+    var id = Number(getQueryStringValue("id"));
+    var sid = Number((getQueryStringValue("subid") == undefined) ? 0 : getQueryStringValue("subid"));
+    getRoute(id, sid);
+    hljs.initHighlightingOnLoad();
 }
 
 
 function getRoutefforNext() {
-    var id = getQueryStringValue("id");
-    getRoute(parseInt(id) + 1);
+    var id = Number(getQueryStringValue("id"));
+    var sid = Number((getQueryStringValue("subid") == undefined) ? 0 : getQueryStringValue("subid"));
+    (sid == 0) ? getRoute(id + 1, sid) : getRoute(id, sid + 1);
 }
 
-function getNextTopic(id, actual_JSON) {
-    var nextId = parseInt(id) + 1;
-    var nextTopic = actual_JSON.filter(data => {
-        return data.id == nextId;
-    })["0"];
-    if (nextTopic == undefined) {
-        nextTopic = actual_JSON.filter(data => {
-            return data.id == 0;
-        })["0"];
-    }
-    document.querySelector("#nextButton").innerHTML = `Next : ${nextTopic.name}  &#x21ac;`;
-}
 function getQueryStringValue(key) {
     return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 }
@@ -38,12 +29,21 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
-function getRoute(id) {
+function getRoute(id, sid) {
     loadJSON(function (response) {
         currentContext: String;
+        currId: Number;
         var actual_JSON = JSON.parse(response);
+        if (sid != 0) {
+            this.currId = Number(sid);
+            actual_JSON = actual_JSON.filter(mainObj => {
+                return mainObj.id == id;
+            })[0].sub;
+        } else {
+            this.currId = Number(id);
+        }
         var active = actual_JSON.filter(data => {
-            return data.id == id;
+            return data.id == (this.currId != '' ? Number(this.currId) : 1);
         });
         if (active["0"] == undefined) {
             this.currentContext = actual_JSON.filter(data => {
@@ -52,26 +52,39 @@ function getRoute(id) {
         } else {
             this.currentContext = active["0"];
         }
+        let mainId = ((sid == 0) ? this.currentContext.id : id);
+        let subId = ((sid != 0) ? this.currentContext.id : sid);
         getNextTopic(this.currentContext.id, actual_JSON);
-        updateURL(this.currentContext.id);
-        callAjax(this.currentContext.screenName);
+        updateURL(mainId, subId);
+        callAjax(this.currentContext);
     });
 }
 
-function callAjax(screenName) {
-    doAjax(screenName);
+function getNextTopic(id, actual_JSON) {
+    var nextId = parseInt(id) + 1;
+    var nextTopic = actual_JSON.filter(data => {
+        return data.id == nextId;
+    })["0"];
+    if (nextTopic == undefined) {
+        nextTopic = actual_JSON.filter(data => {
+            return data.id == 0;
+        })["0"];
+    }
+    document.querySelector("#nextButton").innerHTML = `Next : ${nextTopic.name}  &#x21ac;`;
+}
+
+function callAjax(screen) {
+    doAjax(screen);
 }
 
 
-function doAjax(screenName) {
-
-    var baseURL = `https://kanishkagit.github.io/selfPreparation/WebPages/${screenName}`;
-    //var baseURL = `http://127.0.0.1:5500/WebPages/${screenName}`;
+function doAjax(screen) {
+    var baseURL = `https://kanishkagit.github.io/selfPreparation/WebPages/${screen.screenName}`;
+    //var baseURL = `http://127.0.0.1:5500/WebPages/${screen.screenName}`;
     fetch(baseURL).then(response => {
         response.text().then(data => {
             document.querySelector("#result").innerHTML = data;
-            getContent();
-            getTagsforPage();
+            operateOnPage(screen);
         }).catch(err => {
             console.log("Error is ", err);
         })
@@ -80,9 +93,19 @@ function doAjax(screenName) {
     });
 }
 
-function updateURL(id) {
+function operateOnPage(screen){
+    getContent();
+    getTagsforPage();
+    designPageText();
+    getSubList(screen);
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    });
+}
+
+function updateURL(id, sid) {
     if (history.pushState) {
-        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?id=' + id;
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?id=' + String(id) + '&subid=' + String((sid == 0) ? 0 : sid);
         window.history.pushState({ path: newurl }, '', newurl);
     }
 }
@@ -97,7 +120,7 @@ function getContent() {
             });
             var contentList = '';
             routeJson.forEach(element => {
-                contentList += `<a class="dropdown-item" onclick="getRoute(${element.id})" href='#'>`;
+                contentList += `<a class="dropdown-item" onclick="getRoute(${element.id},0)" href='#'>`;
                 contentList += element.name;
             });
             document.querySelector('#content-table').innerHTML = contentList;
@@ -109,12 +132,60 @@ function getContent() {
 function getTagsforPage() {
     let nodeId = 0;
     let tagsHtml = '';
-    if (document.querySelector('.heading-name') !== null) {
+    if (document.querySelectorAll('.heading-name') != null) {
         document.querySelectorAll('.heading-name').forEach(node => {
             node.setAttribute('id', nodeId + 1);
             tagsHtml += `<a class='btn btn-sm btn-primary mr-1' href='#${nodeId}'>${node.innerHTML}</a>`;
             document.querySelector('#tag-section').innerHTML = tagsHtml
             nodeId++;
         });
+    }
+}
+
+//text highlighting styles
+function designPageText() {
+    let custTxtClass = [
+        {
+            "name": "success",
+            "class": "alert alert-success p-0"
+        },
+        {
+            "name": "warning",
+            "class": "alert alert-warning p-0"
+        },
+        {
+            "name": "danger",
+            "class": "alert alert-danger p-0"
+        },
+        {
+            "name": "info",
+            "class": "alert alert-info p-0"
+        },
+        {
+            "name": "h-text",
+            "class": "h-text"
+        }
+    ];
+    custTxtClass.forEach(classObj => {
+        if (document.querySelectorAll(classObj.name) != null) {
+            document.querySelectorAll(classObj.name).forEach(node => {
+                node.setAttribute('class', classObj.class);
+            });
+        }
+    })
+}
+
+function getSubList(subObj) {
+    if (subObj.sub != 'NA') {
+        let nodeId = 0;
+        let tagsHtml = '';
+        if (subObj.sub != undefined) {
+            subObj.sub.forEach(node => {
+                if (node.id > 0) {
+                    tagsHtml += `<button class='btn btn-lg btn-primary mr-1' onclick="getRoute(${subObj.id},${node.id})">${node.name}</button>`;
+                    document.querySelector('#tag-section').innerHTML = tagsHtml
+                }
+            });
+        }
     }
 }
